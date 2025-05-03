feec7be531744156198034e48f954521a097090f
Caio Castro <caiocastrofrs@gmail.com>
2025-05-03 19:03:48 -0300

    chore: set global font family and initial page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
-  return <div>Hello!</div>;
+export async function getServerSideProps() {
+  return { props: { teste: { a: 1 } } };
+}
+
+export default function Page({ teste }: { teste: { a: number } }) {
+  return <div>{teste.a}</div>;
 }
